59d2ff8b67c58031ff6de61138708ef481211653
Sara <sara.ventura@studenti.unicam.it>
2024-11-20 10:56:45 +0100

    modofica AccountController

--- GiftifyBackend/Giftify/src/main/java/it/unicam/cs/Giftify/Controller/AccountController.java
+++ GiftifyBackend/Giftify/src/main/java/it/unicam/cs/Giftify/Controller/AccountController.java
@@ -7,10 +7,7 @@ import it.unicam.cs.Giftify.Model.Services.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,12 +19,27 @@ public class AccountController {
     @Autowired
     private CommunityService communityService;
 
-    @GetMapping("/getgroups/{id}")
+    @GetMapping("/getCommunities/{userId}")
     public ResponseEntity<List<Community>> getAllCommunities(@PathVariable Long userId) {
         Account account = accountService.getAccountById(userId);
         if (account == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return ResponseEntity.ok(communityService.getAllCommunities());
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<String> joinCommunity (@RequestParam Long userId, @RequestParam String communityId) {
+        try {
+            Account account = accountService.getAccountById(userId);
+            Community community = communityService.getCommunityByAccessCode(communityId);
+            if (community == null || account == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            communityService.addUserToCommunity(account, community);
+            return ResponseEntity.ok("Unione alla community effettuata con successo");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
